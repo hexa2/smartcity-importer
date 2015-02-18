e0fa6f23f75605e409eb765a34d6098d5020e8c4
Robin Ricard <ricard.robin@gmail.com>
2015-02-18 21:52:21 +0100

    transfert

--- src/smartcity_importer/transfert.clj
+++ src/smartcity_importer/transfert.clj
@@ -5,3 +5,14 @@
             [smartcity-importer.time :as t]
             [smartcity-importer.filter :as f]
             [smartcity-importer.destination :as dest]))
+
+(defn transfert-to-influx
+  "Transfert smartcity data source to InfluxDB"
+  [influx-client src-path dest-table src-time-field dest-fields & opts]
+  (dest/write-new-points
+    influx-client dest-table
+    (f/select-fields
+      (t/convert-dates
+        (apply src/get-values src-path opts)
+        :src-field src-time-field)
+      dest-fields)))
