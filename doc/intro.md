8457988c798aa385d7db7eb15d8cf19222317826
Robin Ricard <ricard.robin@gmail.com>
2015-02-18 14:50:28 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to smartcity-importer
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/smartcity_importer/core_test.clj
@@ -0,0 +1,7 @@
+(ns smartcity-importer.core-test
+  (:require [clojure.test :refer :all]
+            [smartcity-importer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
