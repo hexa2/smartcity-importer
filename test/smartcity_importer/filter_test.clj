be868fb5387093270612ac8525f49fbce6d1d9be
Robin Ricard <ricard.robin@gmail.com>
2015-02-23 15:08:01 +0100

    Fix signature change test

--- test/smartcity_importer/filter_test.clj
+++ test/smartcity_importer/filter_test.clj
@@ -4,8 +4,8 @@
 
 (deftest filter-test
   (testing "greater-times with two values"
-    (is (= (greater-times [{:t 0} {:t 10}] 5 :time-field :t)
-           [{:t 10}])))
+    (is (= (greater-times [{:time 0} {:time 10}] 5)
+           [{:time 10}])))
   (testing "select-fields with two values and three fields"
     (is (= (select-fields [{:a 1 :b 2 :c 3} {:a 2 :b 3 :c 4}] [:a :c])
            [{:c 3 :a 1} {:c 4 :a 2}]))))
